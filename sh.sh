cdb649ca6f7b0f136172e32cdf2a099f269ea028
Vašek Ulrich <91714671+qseftqseft@users.noreply.github.com>
2025-07-30 17:02:45 +0200

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,5 +1,5 @@
-wget https://github.com/mdilena/libxp6-binaries/raw/main/binaries/debian_12/libxp6_1.0.2-2_i386.deb
-sudo apt install ./libxp6_1.0.2-2_i386.deb
-
-wget http://security.ubuntu.com/ubuntu/pool/main/g/glibc/multiarch-support_2.27-3ubuntu1.2_i386.deb
-sudo apt install ./multiarch-support_2.27-3ubuntu1.2_i386.deb
+sudo apt install build-essential x11proto-print-dev libx11-dev
+git clone https://github.com/mdilena/libxp6-binaries.git
+cd libxp6-binaries/source
+debuild -b -uc -us
+sudo apt install ../libxp6_*.deb
